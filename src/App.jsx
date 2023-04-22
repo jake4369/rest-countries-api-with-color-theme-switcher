24fc4a4f5b287b109bd1e073e97e470dc95834ce
Jake Samuels <samuels242@googlemail.com>
2023-04-22 11:24:13 +0100

    Add route for Index page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+
+import Layout from "./components/layout/Layout";
+import Index from "./pages/Index";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Index />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
