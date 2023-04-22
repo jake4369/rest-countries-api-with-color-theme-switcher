7bf5c68a9375092a856a6272446503c8ce3f2d5f
Jake Samuels <samuels242@googlemail.com>
2023-04-22 14:07:23 +0100

    Add route to Details pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 
 import Layout from "./components/layout/Layout";
 import Index from "./pages/Index";
+import Details from "./pages/Details";
 
 const App = () => {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<Index />} />
+        <Route path="details/:country_name" element={<Details />} />
       </Route>
     </Routes>
   );
