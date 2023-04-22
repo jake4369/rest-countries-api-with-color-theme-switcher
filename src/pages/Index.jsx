28c88c8571599030b4e4374f145ccc7ed8feb7f7
Jake Samuels <samuels242@googlemail.com>
2023-04-22 12:23:33 +0100

    Render all countries

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from "react";
+
 import SearchBar from "../components/SearchBar";
+import CountryCard from "../components/CountryCard";
 
 const Index = () => {
+  const [allCountries, setAllCountries] = useState([]);
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => {
+        setAllCountries(data);
+      });
+  }, []);
+
+  const allCountryCards = allCountries.map((country) => {
+    return <CountryCard key={country.name.common} country={country} />;
+  });
+
   return (
     <div className="index-page">
       <SearchBar />
+
+      <div className="index-page__flex-container">{allCountryCards}</div>
     </div>
   );
 };
