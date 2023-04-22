205c31ec34de6b0dea2c35e6c9ccb77222ce9a15
Jake Samuels <samuels242@googlemail.com>
2023-04-22 20:22:40 +0100

    BorderCountries component

---
+++ src/components/DetailsPage/BorderCountries.jsx
@@ -0,0 +1,8 @@
+import { useEffect, useState } from "react";
+import { getCountriesData } from "../../utils/api";
+
+const BorderCountries = ({ country }) => {
+  return <div className="border-countries">BorderCountries</div>;
+};
+
+export default BorderCountries;
