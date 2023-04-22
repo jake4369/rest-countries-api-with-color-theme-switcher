5585bbad4b37f422368574e2ef778622b19c463f
Jake Samuels <samuels242@googlemail.com>
2023-04-22 20:24:37 +0100

    Move info columns into separate components

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,9 +1,54 @@
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { BsArrowLeft } from "react-icons/bs";
+import { getCountriesData } from "../utils/api";
+
+import InfoMainColumn from "../components/DetailsPage/InfoMainColumn";
+import InfoSubColumn from "../components/DetailsPage/InfoSubColumn";
 
 const Details = () => {
   const { country_name } = useParams();
+  const [country, setCountry] = useState(null);
 
-  return <div>{country_name} Details</div>;
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getCountriesData(country_name);
+      setCountry(data[0]);
+    };
+    fetchData();
+  }, [country_name]);
+
+  console.log(country);
+
+  return (
+    <div className="details-page">
+      <Link to="/">
+        <button className="back-btn">
+          <BsArrowLeft className="back-btn__arrow" /> Back
+        </button>
+      </Link>
+
+      {country === null ? (
+        <p>Error</p>
+      ) : (
+        <div className="details-page__flex-container">
+          <img
+            src={country.flags.svg || country.flags.png}
+            alt=""
+            className="details-page__flag-img"
+          />
+
+          <div className="details-page__info-container">
+            <div className="details-page__column-container">
+              <InfoMainColumn country={country} />
+
+              <InfoSubColumn country={country} />
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Details;
