afd5fab11617e9c2ea49dd97dcc69eb6d4fa1d7a
Jake Samuels <samuels242@googlemail.com>
2023-04-23 11:26:41 +0100

    Render border countries

--- src/components/DetailsPage/BorderCountries.jsx
+++ src/components/DetailsPage/BorderCountries.jsx
@@ -1,8 +1,52 @@
 import { useEffect, useState } from "react";
-import { getCountriesData } from "../../utils/api";
+import { Link } from "react-router-dom";
+import { getCountriesByCode } from "../../utils/api";
 
 const BorderCountries = ({ country }) => {
-  return <div className="border-countries">BorderCountries</div>;
+  const [borderCountries, setBorderCountries] = useState([]);
+
+  const bordersArr = country?.borders;
+
+  useEffect(() => {
+    const getBorderCountries = async () => {
+      const borderCountriesData = [];
+      for (const border of bordersArr) {
+        const countryData = await getCountriesByCode(border);
+        borderCountriesData.push(...countryData);
+      }
+      setBorderCountries(borderCountriesData);
+    };
+
+    if (bordersArr?.length) {
+      getBorderCountries();
+    }
+  }, [bordersArr]);
+
+  const borderButtons =
+    borderCountries.length > 0 ? (
+      borderCountries.map((country) => {
+        return (
+          <Link
+            key={country.name.common}
+            to={`/details/${country.name.common}`}
+          >
+            <button className="border-countries__btn">
+              {country.name.common}
+            </button>
+          </Link>
+        );
+      })
+    ) : (
+      <p>No bordering countries</p>
+    );
+
+  return (
+    <div className="border-countries">
+      <h2 className="border-countries__heading">Border Countries:</h2>
+
+      <div className="border-countries__btn-container">{borderButtons}</div>
+    </div>
+  );
 };
 
 export default BorderCountries;
