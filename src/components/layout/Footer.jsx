d4c48401f8018231737730249fa624a50bf4e468
Jake Samuels <samuels242@googlemail.com>
2023-04-24 09:32:39 +0100

    Open link in new window

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,7 +3,12 @@ import { BsTwitter } from "react-icons/bs";
 const Footer = () => {
   return (
     <footer>
-      <a href="https://twitter.com/JakeXCode" className="twitter-link">
+      <a
+        href="https://twitter.com/JakeXCode"
+        className="twitter-link"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <BsTwitter className="twitter-icon" /> JakeXCode
       </a>
     </footer>
